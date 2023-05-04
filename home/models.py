7b5154d91f55cfe7cf273ec4dbf48fbffcb659ab
kylemadkins <kyle.m.adkins@gmail.com>
2023-05-04 15:28:06 -0400

    Set up Tailwind, add home page fields

--- home/models.py
+++ home/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    intro_heading = models.CharField(max_length=50, blank=True, null=True)
+    intro_text = RichTextField(blank=True, null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro_heading"),
+        FieldPanel("intro_text")
+    ]
