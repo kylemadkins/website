377bd8cdef41fb475b533cfba6d4f67316bb6fb9
kylemadkins <kyle.m.adkins@gmail.com>
2023-05-04 20:00:43 -0400

    Update Dockerfile and production settings to prep for deployment

--- website/settings/production.py
+++ website/settings/production.py
@@ -1,6 +1,23 @@
+import os
+
 from .base import *
 
 DEBUG = False
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(", ")
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
 
 try:
     from .local import *
