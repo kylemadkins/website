c94011673860eb5590c620211221c171960337a9
kylemadkins <kyle.m.adkins@gmail.com>
2023-05-05 16:59:04 -0400

    Add code block to rich text editor

--- website/blocks.py
+++ website/blocks.py
@@ -2,7 +2,24 @@ from wagtail import blocks
 
 
 class ContentBlock(blocks.StructBlock):
-    content = blocks.RichTextBlock()
+    content = blocks.RichTextBlock(
+        features=[
+            "h2",
+            "h3",
+            "h4",
+            "bold",
+            "italic",
+            "ol",
+            "ul",
+            "hr",
+            "link",
+            "document-link",
+            "image",
+            "embed",
+            "code",
+            "blockquote",
+        ]
+    )
 
     class Meta:
         template = "blocks/content_block.html"
