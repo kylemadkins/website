ba19353af516c04938c61a547a11515ce0f81c5f
kylemadkins <kyle.m.adkins@gmail.com>
2023-05-07 00:45:21 -0400

    Delete files, specify bucket name

--- website/settings/production.py
+++ website/settings/production.py
@@ -31,6 +31,8 @@ AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
+AWS_STORAGE_BUCKET_NAME = "media"
+
 try:
     from .local import *
 except ImportError:
