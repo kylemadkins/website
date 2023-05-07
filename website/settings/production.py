22bbd432b3be331394c5bd35e7f1377030e287ae
kylemadkins <kyle.m.adkins@gmail.com>
2023-05-06 23:56:55 -0400

    Set up production file uploads with Spaces

--- website/settings/production.py
+++ website/settings/production.py
@@ -21,6 +21,16 @@ DATABASES = {
     }
 }
 
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+AWS_S3_REGION_NAME = "nyc3"
+
+AWS_S3_ENDPOINT_URL = "https://kylemadkins.nyc3.digitaloceanspaces.com"
+
+AWS_ACCESS_KEY_ID = "DO00WCZ3AT9CBMY7796W"
+
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
 try:
     from .local import *
 except ImportError:
